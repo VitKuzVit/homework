9b3fbfebc655f7e8a1b3d059ada8587450c9f631
vitvit_x <ykropchik2283@gmail.com>
2022-12-24 10:08:29 +0300

    homework

---
+++ Lesson_1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите число a: ");
+        int a = int.Parse(Console.ReadLine() ?? "0");
+        Console.Write("Введите число b: ");
+        int b = int.Parse(Console.ReadLine() ?? "0");
+
+        if (a == b)
+        {
+            Console.WriteLine("Числа равны.");
+        }
+
+        else if (a > b)
+        {
+            Console.WriteLine("Максимальное число: " + a);
+        }
+
+        else
+        {
+            Console.WriteLine("Максимальное число: " + b);
+        }
